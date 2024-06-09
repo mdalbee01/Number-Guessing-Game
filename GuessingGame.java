9ecc97a810a1295a95b96f7b41c047e99cb22e28
mdalbee01 <168061552+mdalbee01@users.noreply.github.com>
2024-06-10 00:55:04 +0600

    Add files via upload

---
+++ GuessingGame.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.guessinggame;
+
+/**
+ *
+ * @author QC
+ */
+public class GuessingGame {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
